9bedc87dabf5408f0de95dd72bfc645b650371f0
BigBahss <tdennis4496@gmail.com>
2021-02-25 06:22:30 -0500

    Add Operator interface

---
+++ src/Operator.ts
@@ -0,0 +1,87 @@
+import * as vscode from 'vscode';
+import * as util from './utility';
+import { SourceDocument } from './SourceDocument';
+import { CSymbol } from './CSymbol';
+import { SourceSymbol } from './SourceSymbol';
+
+export interface Operator {
+    readonly parent: CSymbol;
+    name: string;
+    returnType: string;
+    parameter: string;
+    body: string;
+    declaration: string;
+    definition(target: SourceDocument, position: vscode.Position, curlySeparator: string): Promise<string>;
+}
+
+export class OpEqual implements Operator {
+    readonly parent: CSymbol;
+    name: string;
+    returnType: string;
+    parameter: string;
+    body: string;
+
+    constructor(parent: CSymbol, memberVariables?: SourceSymbol[]) {
+        this.parent = parent;
+        this.name = 'operator==';
+        this.returnType = 'bool ';
+        this.parameter = 'const ' + parent.name + ' &other';
+        this.body = '';
+        if (memberVariables) {
+            this.setMemberVariables(memberVariables);
+        }
+    }
+
+    get declaration(): string {
+        return this.returnType + this.name + '(' + this.parameter + ') const';
+    }
+
+    async definition(target: SourceDocument, position: vscode.Position, curlySeparator: string): Promise<string> {
+        const eol = target.endOfLine;
+        return this.returnType + await this.parent.scopeString(target, position) + this.name + '(' + this.parameter
+                + ') const' + curlySeparator + '{' + eol + util.indentation() + this.body + eol + '}';
+    }
+
+    setMemberVariables(memberVariables: SourceSymbol[]): void {
+        const eol = util.endOfLine(this.parent.document);
+        const indent = util.indentation();
+
+        if (this.body) {
+            this.body = '';
+        }
+
+        memberVariables.forEach(memberVariable => {
+            this.body += `${memberVariable.name} == other.${memberVariable.name}${eol}${indent}${indent}&& `;
+        });
+
+        if (this.body.length > 3) {
+            this.body = 'return ' + this.body.slice(0, -3).trimEnd() + ';';
+        }
+    }
+}
+
+export class OpNotEqual implements Operator {
+    readonly parent: CSymbol;
+    name: string;
+    returnType: string;
+    parameter: string;
+    body: string;
+
+    constructor(parent: CSymbol) {
+        this.parent = parent;
+        this.name = 'operator!=';
+        this.returnType = 'bool ';
+        this.parameter = 'const ' + parent.name + ' &other';
+        this.body = 'return !operator==(other);';
+    }
+
+    get declaration(): string {
+        return this.returnType + this.name + '(' + this.parameter + ') const';
+    }
+
+    async definition(target: SourceDocument, position: vscode.Position, curlySeparator: string): Promise<string> {
+        const eol = target.endOfLine;
+        return this.returnType + await this.parent.scopeString(target, position) + this.name + '(' + this.parameter
+                + ') const' + curlySeparator + '{' + eol + util.indentation() + this.body + eol + '}';
+    }
+}
