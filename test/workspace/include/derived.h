5802732720e1d671ad02c45a6b71e094b0d9540f
BigBahss <tdennis4496@gmail.com>
2021-04-29 00:12:44 -0400

    Remove standard headers from test workspace so that hopefully the CI stops failing due to cpptools randomly breaking

--- test/workspace/include/derived.h
+++ test/workspace/include/derived.h
@@ -1,6 +1,5 @@
 #include "base.h"
-#include <string>
-#include <iostream>
+#include "string.h"
 
 
 class Derived : public Base
@@ -10,21 +9,19 @@ public:
         : Base(),
           m_description("empty"),
           m_data(42)
-    {
-        std::cout << "Constructed Derived()\n";
-    }
+    { }
 
-    explicit Derived(const std::string &name, const std::string &description = std::string());
+    explicit Derived(const String &name, const String &description = String());
 
     ~Derived();
 
-    int fooBar(const std::string &foo,
+    int fooBar(const String &foo,
                int bar = 47,
-               const std::string &baz = R"(const std::string &baz = "default")");
+               const String &baz = R"(const String &baz = "default")");
 
     constexpr int foo() const;
 
 private:
-    std::string m_description;
+    String m_description;
     const int m_data;
 };
---
+++ test/workspace/include/string.h
@@ -0,0 +1,12 @@
+#ifndef STRING_H
+#define STRING_H
+
+
+class String {
+public:
+    String();
+    String(const String &);
+    String(const char *);
+};
+
+#endif // STRING_H
