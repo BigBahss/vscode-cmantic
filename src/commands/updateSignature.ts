9c136790a5e5c38cfa5c0398073005c29efadb6c
BigBahss <tdennis4496@gmail.com>
2021-05-21 01:49:37 -0400

    Add ability to update trailing function specifiers

--- src/commands/updateSignature.ts
+++ src/commands/updateSignature.ts
@@ -28,59 +28,138 @@ export async function updateSignature(
         }
     }
 
-    const currentSignature = new FunctionSignature(currentFunction);
-    const linkedSignature = new FunctionSignature(linkedFunction);
+    const currentSig = new FunctionSignature(currentFunction);
+    const linkedSig = new FunctionSignature(linkedFunction);
 
     const workspaceEdit = new vscode.WorkspaceEdit();
 
-    updateReturnType(currentSignature, linkedSignature, linkedDoc, workspaceEdit);
-    updateSpecifiers(currentSignature, linkedSignature, linkedDoc, workspaceEdit);
+    updateReturnType(currentSig, linkedSig, linkedDoc, workspaceEdit);
+    updateParameters(currentSig, linkedSig, linkedDoc, workspaceEdit);
+    updateSpecifiers(currentSig, linkedSig, linkedDoc, workspaceEdit);
 
     return vscode.workspace.applyEdit(workspaceEdit);
 }
 
 function updateReturnType(
-    currentSignature: FunctionSignature,
-    linkedSignature: FunctionSignature,
+    currentSig: FunctionSignature,
+    linkedSig: FunctionSignature,
     linkedDoc: SourceDocument,
     workspaceEdit: vscode.WorkspaceEdit
 ): void {
-    if (currentSignature.normalizedReturnType !== linkedSignature.normalizedReturnType) {
-        const nextCharEnd = linkedSignature.returnTypeRange.end.translate(0, 1);
-        const nextChar = linkedDoc.getText(new vscode.Range(linkedSignature.returnTypeRange.end, nextCharEnd));
-        const newReturnType = /[\w\d_]$/.test(currentSignature.returnType) && /^[\w\d_]$/.test(nextChar)
-                ? currentSignature.returnType + ' '
-                : currentSignature.returnType;
-        workspaceEdit.replace(linkedDoc.uri, linkedSignature.returnTypeRange, newReturnType);
+    if (currentSig.normalizedReturnType !== linkedSig.normalizedReturnType) {
+        const nextCharEnd = linkedSig.returnTypeRange.end.translate(0, 1);
+        const nextChar = linkedDoc.getText(new vscode.Range(linkedSig.returnTypeRange.end, nextCharEnd));
+        const newReturnType = /[\w\d_]$/.test(currentSig.returnType) && /^[\w\d_]/.test(nextChar)
+                ? currentSig.returnType + ' '
+                : currentSig.returnType;
+        workspaceEdit.replace(linkedDoc.uri, linkedSig.returnTypeRange, newReturnType);
+    }
+}
+
+function updateParameters(
+    currentSig: FunctionSignature,
+    linkedSig: FunctionSignature,
+    linkedDoc: SourceDocument,
+    workspaceEdit: vscode.WorkspaceEdit
+): void {
+    if (currentSig.parameters.typesEquals(linkedSig.parameters)) {
+        return;
     }
 }
 
 function updateSpecifiers(
-    currentSignature: FunctionSignature,
-    linkedSignature: FunctionSignature,
+    currentSig: FunctionSignature,
+    linkedSig: FunctionSignature,
     linkedDoc: SourceDocument,
     workspaceEdit: vscode.WorkspaceEdit
 ): void {
-    const declaration = parse.maskComments(linkedDoc.getText(linkedSignature.range), true);
-    const startOffset = linkedDoc.offsetAt(linkedSignature.range.start);
+    const declaration = parse.maskComments(linkedDoc.getText(linkedSig.range), true);
+    const startOffset = linkedDoc.offsetAt(linkedSig.range.start);
 
     function removeLeadingSpecifier(re_specifier: RegExp): void {
         const match = declaration.match(re_specifier);
         if (match?.index !== undefined) {
-            const range = linkedDoc.rangeAt(startOffset + match.index, startOffset + match.index + match[0].length);
+            const matchOffset = startOffset + match.index;
+            const range = linkedDoc.rangeAt(matchOffset, matchOffset + match[0].length);
             workspaceEdit.replace(linkedDoc.uri, range, '');
         }
     }
 
-    if (currentSignature.isConstexpr && !linkedSignature.isConstexpr) {
-        workspaceEdit.insert(linkedDoc.uri, linkedSignature.range.start, 'constexpr ');
-    } else if (!currentSignature.isConstexpr && linkedSignature.isConstexpr) {
+    if (currentSig.isConstexpr && !linkedSig.isConstexpr) {
+        workspaceEdit.insert(linkedDoc.uri, linkedSig.range.start, 'constexpr ');
+    } else if (!currentSig.isConstexpr && linkedSig.isConstexpr) {
         removeLeadingSpecifier(/\bconstexpr\b[ \t]*/);
     }
 
-    if (currentSignature.isConsteval && !linkedSignature.isConsteval) {
-        workspaceEdit.insert(linkedDoc.uri, linkedSignature.range.start, 'consteval ');
-    } else if (!currentSignature.isConsteval && linkedSignature.isConsteval) {
+    if (currentSig.isConsteval && !linkedSig.isConsteval) {
+        workspaceEdit.insert(linkedDoc.uri, linkedSig.range.start, 'consteval ');
+    } else if (!currentSig.isConsteval && linkedSig.isConsteval) {
         removeLeadingSpecifier(/\bconsteval\b[ \t]*/);
     }
+
+    let trailingSpecifiers = linkedDoc.getText(linkedSig.trailingSpecifierRange);
+    let maskedSpecifiers = parse.maskComments(trailingSpecifiers, true);
+    maskedSpecifiers = parse.maskRawStringLiterals(maskedSpecifiers, true);
+    maskedSpecifiers = parse.maskQuotes(maskedSpecifiers, true);
+    maskedSpecifiers = parse.maskAttributes(maskedSpecifiers, true);
+    maskedSpecifiers = parse.maskParentheses(maskedSpecifiers, true);
+
+    if (currentSig.isConst && !linkedSig.isConst) {
+        const specifier = /^[\w\d_]/.test(trailingSpecifiers) ? ' const ' : ' const';
+        trailingSpecifiers = specifier + trailingSpecifiers;
+        maskedSpecifiers = specifier + maskedSpecifiers;
+    } else if (!currentSig.isConst && linkedSig.isConst) {
+        const match = maskedSpecifiers.match(/(?<!\n)[ \t]*\bconst\b/);
+        if (match?.index !== undefined) {
+            trailingSpecifiers = trailingSpecifiers.slice(0, match.index)
+                    + trailingSpecifiers.slice(match.index + match[0].length);
+            maskedSpecifiers = maskedSpecifiers.slice(0, match.index)
+                    + maskedSpecifiers.slice(match.index + match[0].length);
+        }
+    }
+
+    if (currentSig.isVolatile && !linkedSig.isVolatile) {
+        const specifier = /^[\w\d_]/.test(trailingSpecifiers) ? ' volatile ' : ' volatile';
+        trailingSpecifiers = specifier + trailingSpecifiers;
+        maskedSpecifiers = specifier + maskedSpecifiers;
+    } else if (!currentSig.isVolatile && linkedSig.isVolatile) {
+        const match = maskedSpecifiers.match(/(?<!\n)[ \t]*\bvolatile\b/);
+        if (match?.index !== undefined) {
+            trailingSpecifiers = trailingSpecifiers.slice(0, match.index)
+                    + trailingSpecifiers.slice(match.index + match[0].length);
+            maskedSpecifiers = maskedSpecifiers.slice(0, match.index)
+                    + maskedSpecifiers.slice(match.index + match[0].length);
+        }
+    }
+
+    if (currentSig.refQualifier !== linkedSig.refQualifier) {
+        if (linkedSig.refQualifier.length !== 0) {
+            trailingSpecifiers = trailingSpecifiers.replace(linkedSig.refQualifier, currentSig.refQualifier);
+            maskedSpecifiers = maskedSpecifiers.replace(linkedSig.refQualifier, currentSig.refQualifier);
+        } else {
+            const match = maskedSpecifiers.match(/^[\s\/\*]*(const|volatile)([\s\/\*]*(const|volatile))?/);
+            if (match) {
+                trailingSpecifiers = trailingSpecifiers.slice(0, match[0].length)
+                        + ' ' + currentSig.refQualifier + trailingSpecifiers.slice(match[0].length);
+                maskedSpecifiers = match[0] + ' ' + currentSig.refQualifier + maskedSpecifiers.slice(match[0].length);
+            } else {
+                trailingSpecifiers = ' ' + currentSig.refQualifier + trailingSpecifiers;
+                maskedSpecifiers = ' ' + currentSig.refQualifier + maskedSpecifiers;
+            }
+        }
+    }
+
+    if (currentSig.normalizedNoexcept !== linkedSig.normalizedNoexcept) {
+        if (linkedSig.normalizedNoexcept.length === 0) {
+            trailingSpecifiers += currentSig.noexcept;
+        } else {
+            const match = maskedSpecifiers.match(/(?<!\n)([ \t]*)\bnoexcept\b(\s*\(\s*\))?/);
+            if (match?.index !== undefined) {
+                trailingSpecifiers = trailingSpecifiers.slice(0, match.index + match[1].length)
+                        + currentSig.noexcept + trailingSpecifiers.slice(match.index + match[0].length);
+            }
+        }
+    }
+
+    workspaceEdit.replace(linkedDoc.uri, linkedSig.trailingSpecifierRange, trailingSpecifiers);
 }
